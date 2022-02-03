59247197f04a86e52a3ac2e9d7cb9d5ecbf38655
jeromekithinji <kithinjijerome@gmail.com>
2022-02-03 12:51:26 +0000

    Added greeting message that changes according to users time

--- src/components/WeatherInfo/WeatherInfo.jsx
+++ src/components/WeatherInfo/WeatherInfo.jsx
@@ -2,13 +2,12 @@ import React, { useState, useEffect } from "react";
 import "./WeatherInfo.scss";
 import { geolocation } from "react-geolocated";
 
-
 const WeatherInfo = () => {
     const [weatherStats, setWeatherStats] = useState(null); //
     const [userLatitude, setUserLatitude] = useState(null);
     const [userLongitude, setUserLongitude] = useState(null);
 
-    const successCallback = (position) => { 
+    const successCallback = (position) => {
         console.log(position);
         console.log(position.coords.latitude);
         console.log(position.coords.longitude);
@@ -23,11 +22,26 @@ const WeatherInfo = () => {
     navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 
     const getWeatherStats = () => {
-        fetch(`http://api.weatherapi.com/v1/current.json?key=e38763e617664e56ab8162340220102&q=${userLatitude,userLongitude}&aqi=no`)
-            .then((response) => response.json()) 
-            .then((jsonResponse) => setWeatherStats(jsonResponse)) 
-            .catch((err) => console.log(err)); 
-            // console.log(weatherStats.location.name);
+        fetch(
+            `http://api.weatherapi.com/v1/current.json?key=e38763e617664e56ab8162340220102&q=${
+                (userLatitude, userLongitude)
+            }&aqi=no`
+        )
+            .then((response) => response.json())
+            .then((jsonResponse) => setWeatherStats(jsonResponse))
+            .catch((err) => console.log(err));
+        // console.log(weatherStats.location.name);
+    };
+
+    const currentHour = new Date().getHours();
+    let greetingTime = "Morning!";
+
+    if (currentHour >= 12) {
+        greetingTime = "Afternoon!";
+    }
+
+    if (currentHour >= 18) {
+        greetingTime = "Evening!";
     }
 
     useEffect(() => {
@@ -36,14 +50,21 @@ const WeatherInfo = () => {
 
     return (
         <div>
-            <p className="">{weatherStats ? weatherStats.location.name : ""}</p> 
+            <header className="greeting">
+                <h1 className="greeting__heading">Good {greetingTime} </h1>
+            </header>
+            <p className="">{weatherStats ? weatherStats.location.name : ""}</p>
             {weatherStats ? <h1>{weatherStats.location.name}</h1> : null}
             {weatherStats ? <h1>{weatherStats.current.temp_c}</h1> : null}
-            {weatherStats ? <h1>{weatherStats.current.condition.text}</h1> : null}
-            {weatherStats ? <img
-                src={weatherStats.current.condition.icon}
-                alt={weatherStats.current.condition.text}
-            /> : null}
+            {weatherStats ? (
+                <h1>{weatherStats.current.condition.text}</h1>
+            ) : null}
+            {weatherStats ? (
+                <img
+                    src={weatherStats.current.condition.icon}
+                    alt={weatherStats.current.condition.text}
+                />
+            ) : null}
             <p className="">{userLatitude}</p>
             <p className="">{userLongitude}</p>
         </div>
