1c278cb31ad756feb341520ebb77114d4169a3a2
jeromekithinji <kithinjijerome@gmail.com>
2022-02-04 11:31:24 +0000

    Finished and styled the weather widget

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import ToDoList from "./components/ToDoList/ToDoList";
 import WeatherInfo from "./components/WeatherInfo/WeatherInfo";
 
 function App() {
     return (
         <div className="App">
-            <WeatherInfo />
+            {/* <WeatherInfo /> */}
+            <ToDoList />
         </div>
     );
 }
--- src/components/WeatherInfo/WeatherInfo.jsx
+++ src/components/WeatherInfo/WeatherInfo.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "./WeatherInfo.scss";
-import { geolocation } from "react-geolocated";
 
 const WeatherInfo = () => {
     const [weatherStats, setWeatherStats] = useState(null); //
@@ -23,15 +22,51 @@ const WeatherInfo = () => {
 
     const getWeatherStats = () => {
         fetch(
-            `http://api.weatherapi.com/v1/current.json?key=e38763e617664e56ab8162340220102&q=${
-                (userLatitude, userLongitude)
-            }&aqi=no`
-        )
+            `http://api.weatherapi.com/v1/current.json?key=e38763e617664e56ab8162340220102&q=${(userLatitude,userLongitude)}&aqi=no`)
             .then((response) => response.json())
             .then((jsonResponse) => setWeatherStats(jsonResponse))
             .catch((err) => console.log(err));
         // console.log(weatherStats.location.name);
     };
+
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+
+    const weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    const date = new Date();
+
+    let minutes =
+        date.getMinutes() < 11 ? "0" + date.getMinutes() : date.getMinutes();
+    let displayDate =
+        weekday[date.getDay()].toUpperCase() +
+        " | " +
+        months[date.getMonth()].toUpperCase().substring(0, 3) +
+        " " +
+        date.getDate() +
+        "  " +
+        date.getHours() +
+        ":" +
+        minutes;
 
     const currentHour = new Date().getHours();
     let greetingTime = "Morning!";
@@ -49,24 +84,24 @@ const WeatherInfo = () => {
     }, []);
 
     return (
-        <div>
-            <header className="greeting">
-                <h1 className="greeting__heading">Good {greetingTime} </h1>
-            </header>
-            <p className="">{weatherStats ? weatherStats.location.name : ""}</p>
-            {weatherStats ? <h1>{weatherStats.location.name}</h1> : null}
-            {weatherStats ? <h1>{weatherStats.current.temp_c}</h1> : null}
-            {weatherStats ? (
-                <h1>{weatherStats.current.condition.text}</h1>
-            ) : null}
-            {weatherStats ? (
-                <img
-                    src={weatherStats.current.condition.icon}
-                    alt={weatherStats.current.condition.text}
-                />
-            ) : null}
-            <p className="">{userLatitude}</p>
-            <p className="">{userLongitude}</p>
+        <div className="widget">
+            <h1 className="greeting">Good {greetingTime} </h1>
+            <div class="weatherIconCont">
+                {weatherStats ? (
+                    <img className="weatherIcon"
+                        src={weatherStats.current.condition.icon}
+                        alt={weatherStats.current.condition.text}
+                    />
+                ) : null}
+            </div>
+            <div class="weatherInfo">
+            {weatherStats ? <div className="temperature"><span>{weatherStats.current.temp_c}&deg;C</span></div> : null}
+                <div class="description">  
+                    {weatherStats ? <h2 className="weatherCondition">{weatherStats.current.condition.text}</h2> : null}  
+                    {weatherStats ? <h2 className="place">{weatherStats.location.name}, {weatherStats.location.country}</h2> : null}
+                </div>
+            </div>
+            <div class="date">{displayDate}</div>
         </div>
     );
 };
